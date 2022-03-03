bdd187fb1e0bae8f11d8f6db7c5b617651908da1
darkjinnee <darkjinnee@gmail.com>
2022-03-04 03:19:15 +0400

    feat: add .env file definition

--- cmd/envporter/main.go
+++ cmd/envporter/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/darkjinnee/go-err"
+	"os"
+	"strings"
+)
+
+var file *os.File
+
+func init() {
+	arg := flag.String(
+		"f",
+		".env",
+		"path to .env file",
+	)
+	flag.Parse()
+	filePath := fmt.Sprintf("%s", *arg)
+
+	dirPath, err := os.Getwd()
+	goerr.Fatal(
+		err,
+		"[Error] envporter.init: Failed to return the root path of directory",
+	)
+
+	if strings.EqualFold(filePath, ".env") {
+		filePath = strings.Join([]string{
+			dirPath,
+			filePath,
+		}, "/")
+	}
+
+	file, err = os.OpenFile(
+		filePath,
+		os.O_RDWR,
+		os.ModePerm,
+	)
+	goerr.Fatal(
+		err,
+		"[Error] envporter.init: Failed to open file",
+	)
+}
 
 func main() {
-	fmt.Print("Hello World!\n")
+	fmt.Print(file.Name() + "\n")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/darkjinnee/envporter
 
 go 1.13
+
+require github.com/darkjinnee/go-err v1.0.0 // indirect
